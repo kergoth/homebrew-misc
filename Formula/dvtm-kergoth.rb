0699c7f271575e00bf20a29f02996095d0f974a6
Christopher Larson <kergoth@gmail.com>
2016-11-09 12:17:56 -0700

    dvtm-kergoth: add my dvtm fork
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ Formula/dvtm-kergoth.rb
@@ -0,0 +1,21 @@
+class DvtmKergoth < Formula
+  desc "Dynamic Virtual Terminal Manager - kergoth's fork"
+  homepage "http://www.brain-dump.org/projects/dvtm/"
+  head "https://github.com/kergoth/dvtm.git", :branch => "integrate"
+
+  # https://github.com/martanne/dvtm/issues/19
+  # depends_on MaximumMacOSRequirement => :yosemite
+
+  conflicts_with "dvtm", :because => "fork of dvtm"
+
+  def install
+    ENV.append_to_cflags "-D_DARWIN_C_SOURCE"
+    system "make", "PREFIX=#{prefix}", "LIBS=-lc -lutil -lncurses", "install"
+  end
+
+  test do
+    result = shell_output("#{bin}/dvtm -v")
+    result.force_encoding("UTF-8") if result.respond_to?(:force_encoding)
+    assert_match(/^dvtm-#{version}/, result)
+  end
+end
